b1be5b76d87d9ab778e70cc69d13abde12e4f684
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-01-14 15:57:58 -0300

    nombres pronunciables y expresivos

---
+++ src/clean-code/01-names.ts
@@ -0,0 +1,66 @@
+(() => {
+
+    // Ejemplo Malos
+    // Archivos a evaluar - files to evaluate
+    const fs = [
+        { id: 1, f: false },
+        { id: 2, f: false },
+        { id: 3, f: true },
+        { id: 4, f: false },
+        { id: 5, f: false },
+        { id: 7, f: true },
+    ]; 
+    
+    // Archivos marcados para borrar - files to delete
+    const arhivos = fs.map( f => f.f );
+
+    // Ejemplo Mejor
+    // Archivos a evaluar - files to evaluate
+    const filesToEvualate = [
+        { id: 1, flagged: false },
+        { id: 2, flagged: false },
+        { id: 3, flagged: true },
+        { id: 4, flagged: false },
+        { id: 5, flagged: false },
+        { id: 7, flagged: true },
+    ]; 
+    
+    // Archivos marcados para borrar - files to delete
+    const filesToDelete = filesToEvualate.map( file => file.flagged );
+
+    //Malos
+    class AbstractUser { };
+    class UserMixin { };
+    class UserImplementation { };
+    interface IUser { };
+
+    // Mejor
+    class User { };
+    interface User { };
+
+
+    // Todo: Tarea
+        
+    // día de hoy - today
+    const ddmmyyyy = new Date();
+    
+    // días transcurridos - elapsed time in days
+    const d: number = 23;
+    
+    // número de archivos en un directorio - number of files in directory
+    const dir = 33;
+    
+    // primer nombre - first name
+    const nombre = 'Fernando';
+    
+    // primer apellido - last name
+    const apellido = 'Herrera';
+
+    // días desde la última modificación - days since modification
+    const dsm = 12;
+    
+    // cantidad máxima de clases por estudiante - max classes per student
+    const max = 6;
+
+
+})();
