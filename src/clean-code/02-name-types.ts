ac7a313437b0ace2717650f5639029029f678df0
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-01-15 16:55:48 -0300

    nombres de funciones, argumentos y parametros

--- src/clean-code/02-name-types.ts
+++ src/clean-code/02-name-types.ts
@@ -1,5 +1,6 @@
 (() => {
 
+    // MAL EJEMPLOS
     // arreglo de temperaturas celsius
     const arrayOfNums = [33.6, 12.34];
 
@@ -27,7 +28,6 @@
     // Tiempo al final
     const end = new Date().getTime() - start;
 
-
     // Funciones
     // Obtiene los libros
     function book() {
@@ -49,5 +49,54 @@
         throw new Error('Function not implemented.');
     }
     
+    // BUENOS EJEMPLOS
+    // arreglo de temperaturas celsius
+    const temperaturesCelsius = [33.6, 12.34];
+
+    // Dirección ip del servidor
+    const serverIp = '123.123.123.123';
+
+    // Listado de usuarios
+    const users = [{id: 1, email: 'fernando@google.com'},{ id: 2, email: 'juan@google.com' }, { id: 3, email: 'melissa@google.com' }];
+
+    // Listado de emails de los usuarios
+    const usersEmails = users.map( user => user.email );
+
+    // Variables booleanas de un video juego
+    const canJump = false;
+    const canRun = true;
+    const hasItems = true;
+    const isLoading = false;
+
+    // Otros ejercicios
+    // tiempo inicial
+    const startTime = new Date().getTime();
+    //....
+    // 3 doritos después
+    //...
+    // Tiempo al final
+    const endTime = new Date().getTime() - startTime;
+
+
+    // Funciones
+    // Obtiene los libros
+    function getBooks() {
+        throw new Error('Function not implemented.');
+    }
+
+    // obtiene libros desde un URL
+    function getBooksByUrl( u: string) {
+        throw new Error('Function not implemented.');
+    }
+    
+    // obtiene el área de un cuadrado basado en sus lados
+    function getSquareArea( s: number ) {
+        throw new Error('Function not implemented.');
+    }
+
+    // imprime el trabajo
+    function printJob() {
+        throw new Error('Function not implemented.');
+    }
 })();
 
